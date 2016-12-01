64b5e8af9bd1794aa32d83e0368def35a0cd48be
pechentw <pechen.tw@gmail.com>
2016-12-01 20:25:41 +0800

    Initial commit

---
+++ app/src/main/java/com/soho/yvtc/yvn120101a/MainActivity.java
@@ -0,0 +1,13 @@
+package com.soho.yvtc.yvn120101a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/soho/yvtc/yvn120101a/MyService.java
@@ -0,0 +1,25 @@
+package com.soho.yvtc.yvn120101a;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by yvtc on 2016/12/1.
+ */
+
+public class MyService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FCM", "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+    }
+}
